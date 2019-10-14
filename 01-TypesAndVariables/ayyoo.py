6ae930f3acc19ae28304d1c580d14f4592225fc2
s-115-10 <s-115-10@gumiforest.ae.krakow.pl>
2019-10-14 14:45:38 +0200

    01

---
+++ 01-TypesAndVariables/ayyoo.py
@@ -0,0 +1,35 @@
+"""# z.10
+x=7
+y=34
+print("x = ",x)
+print("y = ",y)
+
+z=7
+x=34
+y=z
+print("x = ",x)
+print("y = ",y)
+#z.11
+liczby = [5,1,8,6,3]
+    #a 
+
+    #b
+wynik=0
+for i in liczby:
+    wynik += i*i
+print(wynik)
+    #f
+print(liczby[0]%3)
+    #h
+print(liczby[3]<<2)
+#z.13
+imiona=["Jan","Dariusz","Marcin"]
+    #a
+print(len(imiona))
+    #b
+print(imiona[0])
+    #c
+print(imiona[len(imiona)-1])"""
+# z.15
+liczba=int(input())
+print(f'Warosc liczby to {liczba},a {liczba**2} to jej potega')
